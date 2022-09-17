d1dc742c8ac9a379bf77787de4bcbbefc38adf0c
Shashank <metkar711@gmail.com>
2022-09-17 15:58:09 +0530

    hey

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app=express();
 app.set('view engine','ejs');
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static("public"));
 
 
