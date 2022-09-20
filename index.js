33866d999b7419348e1427526c775f3111959e92
Shashank <metkar711@gmail.com>
2022-09-20 14:54:34 +0530

    changed againn

--- index.js
+++ index.js
@@ -81,11 +81,7 @@ app.get("/compose", function(req, res){
 	res.redirect("/home");
   });
 
-  let port=process.env.PORT;
-  if(port ==null || port ==""){
-	port=3000;
-  }
- 
-app.listen(port,function(){
-	console.log("server is running on port 3000");
-});
+
+  app.listen(process.env.PORT || 3000, function(){
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
