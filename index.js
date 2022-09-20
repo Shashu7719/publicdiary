c1a2b9417aee3576e598e852a26539b83642ade3
Shashu7719 <110917196+Shashu7719@users.noreply.github.com>
2022-09-20 19:10:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 app.set('view engine','ejs');
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:false}));
+// app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static("public"));
 
 const MONGO_DB=process.env.MONGO_DB;
